b6e30ba65241dd85f791bed408f4f33dd7ce5987
Zayed Khan <zayedkh02@gmail.com>
2025-02-12 12:06:54 +0000

    Added class to represent event objects and also added interface 'CalenderDataProvider' to provide to other sectors for data sharing.

---
+++ Lancaster/src/CalenderDataProvider.java
@@ -0,0 +1,7 @@
+import java.util.List;
+public interface CalenderDataProvider {
+    List<Event> getAllEvents();
+    List<Event> getAvailableSlots();
+    Event getEventByID(String eventId);
+    boolean reserveSlot(String eventId, int days);
+}
