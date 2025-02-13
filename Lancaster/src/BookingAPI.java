19bef947af66257dccf4c9aea2c5fd99126ffe46
Zayed <zayedkh02@gmail.com>
2025-02-13 19:54:54 +0000

    Removed Booking class - redundant - changed to Event class instead to represent a booking

--- Lancaster/src/BookingAPI.java
+++ Lancaster/src/BookingAPI.java
@@ -1,7 +1,7 @@
 // API for managing booking functions, this allows marketing to book large groups & meetings
 // Can be used by marketing and box office
 public interface BookingAPI {
-   Booking  createBooking(int bookingId, String newDetails);
+   Event createEvent(String eventId, int days, String status, String eventType, double cost);
    void updateBooking(int bookingId, String newDetails);
    void cancelBooking(int bookingId);
 }
