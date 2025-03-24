ef1761f117b6dc9ae7d8c180160d39b46dd1a95c
Zayed <zayedkh02@gmail.com>
2025-03-24 01:24:20 +0000

    Added req focus

--- LancasterFX/src/main/java/com/lancaster/controller/LogInController.java
+++ LancasterFX/src/main/java/com/lancaster/controller/LogInController.java
@@ -1,11 +1,13 @@
 package com.lancaster.controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -19,9 +21,12 @@ public class LogInController implements Initializable {
     private TextField tf_password;
     @FXML
     private Button btn_login;
+    @FXML
+    private Label label_welcome;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        Platform.runLater(() -> label_welcome.requestFocus());
         btn_login.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
