2c2dfb64f45f3c5212a83d8631b5a303e80657cb
Zayed <zayedkh02@gmail.com>
2025-03-30 16:33:23 +0100

    Added db functionality + password checking

--- LancasterFX/src/main/java/com/lancaster/controller/LogInController.java
+++ LancasterFX/src/main/java/com/lancaster/controller/LogInController.java
@@ -1,15 +1,18 @@
 package com.lancaster.controller;
 
+import com.lancaster.utils.DBUtils;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.time.YearMonth;
 import java.util.ResourceBundle;
@@ -31,13 +34,8 @@ public class LogInController implements Initializable {
             @Override
             public void handle(ActionEvent actionEvent) {
                 try {
-                    FullCalendarView calendarView = new FullCalendarView(YearMonth.now());
-                    if (!tf_password.getText().isEmpty()) {
-                        Stage primaryStage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
-                        primaryStage.getScene().setRoot(calendarView.getView());
-                        primaryStage.show();
-                    }
-                } catch (Exception e) {
+                    DBUtils.loginUser(actionEvent, tf_password.getText());
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
