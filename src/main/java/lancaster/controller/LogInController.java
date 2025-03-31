23bc328f152ac4ae59966cecf2111cdf80dd6333
Zayed Khan <157996027+ZayedKh@users.noreply.github.com>
2025-03-31 11:06:59 +0100

    Revert "Added multiple booking to the calendar, a selection pane and daily sheets"

--- src/main/java/lancaster/controller/LogInController.java
+++ src/main/java/lancaster/controller/LogInController.java
@@ -1,33 +1,22 @@
 package lancaster.controller;
 
-<<<<<<< HEAD:LancasterFX/src/main/java/com/lancaster/controller/LogInController.java
-=======
 import lancaster.utils.DBUtils;
->>>>>>> main:src/main/java/lancaster/controller/LogInController.java
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-<<<<<<< HEAD:LancasterFX/src/main/java/com/lancaster/controller/LogInController.java
-import javafx.scene.Parent;
-=======
->>>>>>> main:src/main/java/lancaster/controller/LogInController.java
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
-<<<<<<< HEAD:LancasterFX/src/main/java/com/lancaster/controller/LogInController.java
-import javafx.scene.control.PasswordField;
-import javafx.stage.Stage;
-=======
->>>>>>> main:src/main/java/lancaster/controller/LogInController.java
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LogInController implements Initializable {
-
     @FXML
-    private PasswordField tf_password;
+    private TextField tf_password;
     @FXML
     private Button btn_login;
     @FXML
@@ -36,20 +25,14 @@ public class LogInController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Platform.runLater(() -> label_welcome.requestFocus());
-        btn_login.setOnAction((ActionEvent event) -> {
-            try {
-                if (!tf_password.getText().isEmpty()) {
-                    Stage primaryStage = (Stage) btn_login.getScene().getWindow();
-                    // Adjust the resource path if needed:
-                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/lancaster/ui/SelectionPane.fxml"));
-                    Parent selectionPane = loader.load();
-                    primaryStage.getScene().setRoot(selectionPane);
-                    primaryStage.show();
-                } else {
-                    System.out.println("Password field is empty.");
+        btn_login.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                try {
+                    DBUtils.loginUser(actionEvent, tf_password.getText());
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
         });
     }
