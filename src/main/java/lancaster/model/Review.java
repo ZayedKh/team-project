bfbd696f9861b8b650f9f64a7809cc07f661125e
Mohib866 <158054690+Mohib866@users.noreply.github.com>
2025-04-02 17:54:58 +0100

    Made a fully functioning review ui.

--- src/main/java/lancaster/model/Review.java
+++ src/main/java/lancaster/model/Review.java
@@ -1,62 +1,76 @@
 package lancaster.model;
 
-public abstract class Review {
-    // Review class - has 5 attributes currently.
+public class Review {
+    private int reviewID;
     private int rating;
-    private int reviewId;
     private String author;
     private String title;
     private String description;
+    private String room;
+    private String timestamp;
 
-
-    // constructor to initialize new review object
-    Review(int rating, int reviewId, String author, String title, String description) {
-        this.reviewId = reviewId;
+    public Review(int reviewID, int rating, String author, String title, String description, String room) {
+        this.reviewID = reviewID;
         this.rating = rating;
         this.author = author;
         this.title = title;
         this.description = description;
+        this.room = room;
     }
 
+    public int getReviewID() {
+        return reviewID;
+    }
 
-    // Getters and Setters.
+    public void setReviewID(int reviewID) {
+        this.reviewID = reviewID;
+    }
+
     public int getRating() {
         return rating;
-    }
-
-    public int getReviewId() {
-        return reviewId;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setReviewId(int reviewId) {
-        this.reviewId = reviewId;
     }
 
     public void setRating(int rating) {
         this.rating = rating;
     }
 
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
     }
 }
