1a5f711dcbaebca208206c671c9b272751b9bf86
Zayed Khan <zayedkhan@zayeds-mbp.home>
2025-04-04 20:57:43 +0100

    Small changes

--- src/main/java/lancaster/boxOfficeInterface/JDBC.java
+++ src/main/java/lancaster/boxOfficeInterface/JDBC.java
@@ -8,19 +8,17 @@ public class JDBC {
     private final Connection connection;
 
 
-    public JDBC() throws SQLException {
+    public JDBC() throws SQLException, ClassNotFoundException {
         String url = "jdbc:mysql://localhost:3306/";
         String username = "in2033t44_a";
         String password = "wcYtgG2jphQ";
-        
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
         this.connection = DriverManager.getConnection(url, username, password);
     }
 
+    public static void main(String[] args) {
 
+    }
 }
